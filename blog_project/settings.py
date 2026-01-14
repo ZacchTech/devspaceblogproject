188c04e7aa231527db673492bbfc1ec0915e2210
Alade Zaccheous <aladezaccheous52@gmail.com>
2026-01-14 15:45:35 +0100

    ALLOWED_HOSTS

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7iumgabzf&)a*uhgrrm@_mc_lnay&!$!4!7!%o%agg&qjzc9(@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+#'django-insecure-7iumgabzf&)a*uhgrrm@_mc_lnay&!$!4!7!%o%agg&qjzc9(@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['devspaceblogproject.onrender.com']
 
 
 # Application definition
