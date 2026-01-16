9fb59f60d09f61638bbf6782b93e2adf9e5b16bc
Alade Zaccheous <aladezaccheous52@gmail.com>
2026-01-16 14:29:47 +0100

    Fix manifest strictness and media urls

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'), 
@@ -41,4 +40,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    
