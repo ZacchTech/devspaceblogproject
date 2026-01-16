ba83a4a74211d7c601264fe6e003f83d3ac8c784
Alade Zaccheous <aladezaccheous52@gmail.com>
2026-01-16 16:23:32 +0100

    whitenoise.storage.CompressedStaticFilesStorage

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # --- STATIC AND MEDIA SETTINGS ---
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STORAGES = {
     "default": {
